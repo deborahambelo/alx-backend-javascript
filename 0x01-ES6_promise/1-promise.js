cad73d23e8c7bfaba4762bdd472594358f58a54e
deborahambelo <ketema.deborah@gmail.com>
2024-08-07 19:21:11 +0000

    task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,12 @@
 export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => 
-    if (success === true) {
+  return new Promise((resolve, reject) => { // create a new promise
+    if (success === true) { // if the promise resolves, return the response
       resolve({
         status: 200,
         body: 'Success',
       });
-    } else {
+    } else { // if the promise rejects, return the error
       reject(new Error('The fake API is not working currently'));
     }
   });
-}
+} // log a message to the console when the promise is resolved or rejected
