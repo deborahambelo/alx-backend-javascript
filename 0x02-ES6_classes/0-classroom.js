066cc351a3d5126ec7b9ecd6020198098f3981f0
deborahambelo <101412212+deborahambelo@users.noreply.github.com>
2024-08-22 22:33:54 +0530

    task 0

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,6 @@
+export default class ClassRoom { // Prototype class for ClassRoom
+// Constructor function for ClassRoom class with parameter maxStudentsSize (number)
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize; // Private variable for maxStudentsSize
+  }
+}
